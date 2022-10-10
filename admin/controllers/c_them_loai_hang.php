3f91f7b20f2e7138c16dec7ec5107e16b3530c93
haudv3202 <haudang02.it@gmail.com>
2022-10-10 20:28:56 +0700

    upload type_products,product,user/70%

--- admin/controllers/c_them_loai_hang.php
+++ admin/controllers/c_them_loai_hang.php
@@ -1,7 +1,7 @@
 <?php
 class c_quan_li_loai_hang {
     public function index() {
-        $view = "views/quan_li_hang/v_quan_li_loai_hang.php";
+        $view = "views/quan_li_hang/v_them_loai_hang.php";
         include ("templates/layout.php");
     }
 
@@ -11,10 +11,15 @@ class c_quan_li_loai_hang {
 
     }
 
-
     public function edit_loai_hang() {
         $view = "views/quan_li_hang/v_edit_loai_hang.php";
         include ("templates/layout.php");
 
+    }
+
+    public function add_loai_hang(){
+        if(isset($_POST[""])){
+
+        }
     }
 }
