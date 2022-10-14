406027b87cc577dc76f003159b82fdb76c5e98b3
haudv3202 <haudang02.it@gmail.com>
2022-10-14 12:23:07 +0700

    update admin (accouts,comments,static),user (top products like,detail products,product type,product view)

--- views/register_login/v_sign_in.php
+++ views/register_login/v_sign_in.php
@@ -6,45 +6,70 @@
             </script>";
     }
 ?>
-<div class="main">
-<!-- Sign in  Form -->
-<section class="sign-in">
-    <div class="container">
-        <div class="signin-content">
-            <div class="signin-image">
-                <figure><img src="public/layout_resgister_login/images/signin-image.jpg" alt="sing up image"></figure>
-                <a href="register.php" class="signup-image-link">Create an account</a>
-            </div>
 
-            <div class="signin-form">
-                <h2 class="form-title">Sign up</h2>
-                <form method="POST" action="" class="register-form" id="login-form">
-                    <div class="form-group">
-                        <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                        <input type="email" name="your_email" id="your_name" placeholder="Your Email"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                        <input type="password" name="your_pass" id="your_pass" placeholder="Password"/>
-                    </div>
-                    <div class="form-group">
-                        <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
-                        <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
-                    </div>
-                    <div class="form-group form-button">
-                        <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
-                    </div>
-                </form>
-                <div class="social-login">
-                    <span class="social-label">Or login with</span>
-                    <ul class="socials">
-                        <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
-                        <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter"></i></a></li>
-                        <li><a href="#"><i class="display-flex-center zmdi zmdi-google"></i></a></li>
-                    </ul>
+<body>
+<div class="container">
+
+    <div class="form login" id="form">
+        <div class="content">
+            <form action="" method="POST" >
+                <h1>Login</h1>
+                <div class="group">
+                    <input type="text" id="username-login" name="your_email" class="inputText" placeholder="&nbsp;" required>
+                    <label for="username-login">Username</label>
                 </div>
-            </div>
+                <div class="group">
+                    <input type="password" id="pass-login"  name="your_pass" class="inputText" placeholder="&nbsp;" required>
+                    <label for="pass-login">Password</label>
+                </div>
+                <div class="group check">
+                    <input type="checkbox"> Save login
+                </div>
+                <div class="group">
+
+                </div>
+                <button type="submit"  name="signin">Login</button>
+                <p><a href="index.php">No, back to home</a></p>
+            </form>
+        </div>
+
+
+        <div class="content">
+            <form action="" method="POST" enctype="multipart/form-data">
+                <h1>Register</h1>
+                <div class="group">
+                    <input type="text" id="username-reg"  name="name" class="inputText" placeholder="&nbsp;" required>
+                    <label for="username-reg">Username</label>
+                </div>
+                <div class="group">
+                    <input type="email" id="email-reg" name="email" class="inputText" placeholder="&nbsp;" required>
+                    <label for="email-reg">Email</label>
+                </div>
+                <div class="group">
+                    <input type="password" id="pass-reg" name="pass" class="inputText" placeholder="&nbsp;" required>
+                    <label for="pass-reg">Password</label>
+                </div>
+                <div class="group">
+                    <input type="password" id="repass-reg" name="re_pass" class="inputText" placeholder="&nbsp;" required>
+                    <label for="pass-reg">Repassword</label>
+                </div>
+                <div class="group">
+                    <!-- <label for="">Image</label> -->
+                    <input type="file" name="image" class="inputFile">
+                </div>
+                <button type="submit" name="register" >Login</button>
+                <p><a href="sign_in.php">I already have an account </a></p>
+            </form>
+        </div>
+
+
+        <div class="form-rotate">
+            <div id="rotate"></div>
         </div>
     </div>
-</section>
+    <div class="option">
+        <div class="bg-active" id="bg-active"></div>
+        <div class="changeType active" id="login">Login</div>
+        <div class="changeType" id="register">Register</div>
+    </div>
 </div>
